e511dc924a7685ae98ac7aa51da70bf5b1cb3022
bpereda <106450242+bpereda@users.noreply.github.com>
2025-05-19 17:06:47 -0300

    Config for GCP

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,26 @@ import * as YAML from 'yaml';
 import { writeFileSync } from 'fs';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
-    app.use((req, res, next) => {
-        console.log(`[${req.method}] ${req.originalUrl}`);
-        next();
+    const app = await NestFactory.create(AppModule, { 
+        cors: true,
+        logger: process.env.NODE_ENV === 'production' 
+            ? ['error', 'warn'] 
+            : ['error', 'warn', 'log', 'debug']
     });
-    const config = new DocumentBuilder()
-        .setTitle('GeoVacas API')
-        .setDescription('Documentación relacionada a los endpoints de la API')
-        .setVersion('1.0')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    const yamlString = YAML.stringify(document);
-    writeFileSync('./GeoVacasEndpoints.yaml', yamlString);
-    SwaggerModule.setup('api', app, document);
-    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+    if (process.env.NODE_ENV !== 'production') {
+        const config = new DocumentBuilder()
+            .setTitle('GeoVacas API')
+            .setDescription('Documentación relacionada a los endpoints de la API')
+            .setVersion('1.0')
+            .build();
+        const document = SwaggerModule.createDocument(app, config);
+        const yamlString = YAML.stringify(document);
+        writeFileSync('/tmp/GeoVacasEndpoints.yaml', yamlString);
+        SwaggerModule.setup('api', app, document);
+    }
+
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
